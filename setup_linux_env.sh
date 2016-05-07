d6bba9131f91315734a4fe3730b844409b06e120
zinkin <justin.ting@foxmail.com>
2016-05-08 00:58:54 +0800

    update vim/git configuration

---
+++ setup_linux_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+setup_vim(){
+	# move colors
+	mkdir -p ~/.vim
+	cp -rf ./vim/colors ~/.vim
+	# move vimrc
+	if [ -e ~/.vimrc ];then
+		cp ~/.vimrc ~/.vimrc-bk-`date +%Y%m%d-%H%M%S`
+	fi
+	cp ./vim/vimrc ~/.vimrc
+}
+
+setup_vim
